8b965e84d242ec97e803584ce0b32260e59e9b70
AleksanderLib <102597150+AleksanderLib@users.noreply.github.com>
2024-05-25 17:11:24 +0300

    commit_1

--- src/main/java/differenceFly.java
+++ src/main/java/differenceFly.java
@@ -1,28 +1,29 @@
 import java.util.Comparator;
 import java.util.List;
 // Класс для разницы между средней ценой и медианой
-public class differenceFly extends FlightJson {
+public class differenceFly {
     private double averagePrice;
     private  int medianDuration;
     private double difference;
     public differenceFly() {
-        averagePrice = calculateAveragePrice(tickets.getFlights());
-        medianDuration = calculateMedianPrice(tickets.getFlights());
+        FlightJson flightJson = new FlightJson();
+        averagePrice = calculateAveragePrice(flightJson.tickets.getFlights());
+        medianDuration = calculateMedianPrice(flightJson.tickets.getFlights());
         difference = averagePrice - medianDuration;
     }
 
 
-    private static double calculateAveragePrice(List<Flight> flights) {
+    private static double calculateAveragePrice(List<FlightJson.Flight> flights) {
         double totalPrice = 0;
-        for (Flight flight : flights) {
+        for (FlightJson.Flight flight : flights) {
             totalPrice += flight.getPrice();
         }
         flights.size();
         return totalPrice / flights.size();
     }
 
-    private static int calculateMedianPrice(List<Flight> flights) {
-        flights.sort(Comparator.comparingInt(Flight::getPrice));
+    private static int calculateMedianPrice(List<FlightJson.Flight> flights) {
+        flights.sort(Comparator.comparingInt(FlightJson.Flight::getPrice));
         if (flights.size() % 2 == 0) {
             int index = flights.size() / 2;
             return (flights.get(index - 1).getPrice() + flights.get(index).getPrice()) / 2;
