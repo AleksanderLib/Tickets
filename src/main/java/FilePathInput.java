70c7a7a9b2b3d995eac6803061369852a4a933e8
AleksanderLib <102597150+AleksanderLib@users.noreply.github.com>
2024-05-27 21:15:59 +0300

    new_jar

--- src/main/java/FilePathInput.java
+++ src/main/java/FilePathInput.java
@@ -1,21 +1,29 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
 public class FilePathInput {
-    private  String filePath;
+    private String filePath;
 
     public FileReader getFile() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите путь к файлу 'tickets.json': ");
-        try {
-            filePath = scanner.nextLine();
-            return new FileReader(filePath);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Файл не найден по указанному пути.");
-            return null;
+        boolean fileFound = false;
+        while (!fileFound) {
+            System.out.print("Введите путь к файлу 'tickets.json': ");
+            try {
+                filePath = scanner.nextLine();
+                File file = new File(filePath);
+                if (file.exists()) {
+                    fileFound = true;
+                    return new FileReader(file);
+                } else {
+                    System.out.println("Файл не найден по указанному пути.");
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
+        return null;
     }
 }
-
